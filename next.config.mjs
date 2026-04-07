de59afeb3b1ad24983423fbc762ec1adeb5d8482
Samuel <samawyck@gmail.com>
2026-04-07 14:56:04 -0500

    Config for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+
+
 const nextConfig = {
-  /* config options here */
+	/* config options here */
+	output: "export"
 };
+
+
 
 export default nextConfig;
