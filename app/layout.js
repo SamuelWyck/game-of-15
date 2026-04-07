ee29526e7d82a7e97280390021870ec1b9d689df
Samuel <samawyck@gmail.com>
2026-04-07 14:54:33 -0500

    Add header to layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 
 
@@ -21,7 +22,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-		<body>{children}</body>
+		<body>
+			<Header/>
+			{children}
+		</body>
 		</html>
 	);
 }
