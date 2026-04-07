4ed58be078d50a470911ea8d61cd160e535c9722
Samuel <samawyck@gmail.com>
2026-04-07 15:22:42 -0500

    Create home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 
 
 export default function Home() {
 	return (
-		<div>Hi</div>
+		<main className={styles["homepage-main"]}>
+			<Link href={"/game"} className={styles["game-card-link"]}>
+			<div className={styles["game-card"]}>
+				<div className={styles["img-wrapper"]}>
+					<img src="/paper.jpg" alt="paper" />
+				</div>
+				<div className={styles["game-card-title-wrapper"]}>
+					<p className={styles["game-card-title"]}>Play Game of 15</p>
+				</div>
+			</div>
+			</Link>
+		</main>
 	);
 };
