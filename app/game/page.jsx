dbc7283c7323cbdc6ca395156cc785834ba12df7
Samuel <samawyck@gmail.com>
2026-04-07 15:31:21 -0500

    Stub out game page

---
+++ app/game/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+
+
+function GamePage() {
+    return (
+        <main className={styles["gamepage-main"]}>game page</main>
+    );
+};
+
+
+
+export default GamePage;
